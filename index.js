88db4df3bf725a07f39c24e356f1a64b4330b0f7
Fewnikz <fewnikz@proton.me>
2026-09-21 12:37:56 +0200

    Made a small bit javascript

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const homes = document.getElementById("homes");
 
-function createColElement(setText){
-    let createdDiv = document.createElement("div");
-    createdDiv.className = "col";
+// Function that creates the cards for the homes
+function createColElement(setText) {
+    // Column element
+    let createdCol = document.createElement("div");
+    createdCol.className = "col";
 
-    let createdP = document.createElement("p")
-    createdP.textContent = setText
+    // A element
+    let createdA = document.createElement("a");
+    createdA.href = "bolig-detaljer.html";
+    createdA.className = "bolig-kort";
 
-    homes.appendChild(createdDiv)
-    createdDiv.appendChild(createdP)
+    homes.appendChild(createdCol)
+    createdCol.appendChild(createdA)
 }
 
 async function createHomes() {
@@ -19,9 +23,18 @@ async function createHomes() {
 
     const json = await response.json();
 
-    for(let i = 0; i < 15; i++) {
+    for (let i = 0; i < 15; i++) {
         createColElement(json[i].adresse)
     }
 }
+
+//<a href="bolig1.html" className="bolig-kort">
+//    <img src="https://picsum.photos/id/1040/400/300" alt="Hus på Blomstervej 12"/>
+//    <div className="bolig-kort-tekst">
+//        <h2>Blomstervej 12, København</h2>
+//        <p>85 m²</p>
+//        <p className="pris">2.495.000 kr.</p>
+//    </div>
+//</a>
 
 createHomes();
